68d713f090e204f8163f06bc22318f832bc425f9
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-09-16 11:35:32 +0300

    fixed-final

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -48,6 +49,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
