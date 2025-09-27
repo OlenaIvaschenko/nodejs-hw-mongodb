3097b58bf1f8c99941dcb6b4c575db39b1f32058
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-09-27 23:15:42 +0300

    first part

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,18 +9,21 @@ import {
 } from '../controllers/contacts.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId',isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', ctrlWrapper(createStudentController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createStudentController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', isValidId, ctrlWrapper(patchContactController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 router.use((req, res, next) => {
   res.status(404).json({ status: 404, message: 'Not found' });
