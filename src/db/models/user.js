674ebf3ed30acbf9c8ad9ed07be2fc0babc0f231
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-10-02 23:59:56 +0300

    start

---
+++ src/db/models/user.js
@@ -0,0 +1,10 @@
+import {model, Schema} from 'mangoose';
+const usersSchema = new Schema (
+    {
+name: { type: String, required: true },
+email: { type: String, required: true, unique: true },
+password: { type: String, required: true }
+    },
+    {timestamps: true, versionKey: false },
+);
+export const UsersCollection = model ('users', usersSchema); 
