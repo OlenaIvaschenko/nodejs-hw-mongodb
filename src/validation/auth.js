eef4428684b0a95c399a4c11ee0a98bbac2b6b7e
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-10-19 01:32:05 +0300

    post/contacts

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,21 +1,20 @@
 import Joi from 'joi';
 export const registerUserSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-}) 
+});
 
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-}) 
+});
 
-export const sendResetEmailSchema = Joi.object ({
- email: Joi.string().email().required(),
+export const sendResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
 });
 
 export const resetPasswordSchema = Joi.object({
   token: Joi.string().required(),
-   password: Joi.string().required(),
-
-})
+  password: Joi.string().required(),
+});
